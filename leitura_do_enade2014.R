d7848383658946b61628a60f5827a6a2592ece9f
Neylson Crepalde <neylsoncrepalde@gmail.com>
2019-09-24 10:25:01 -0300

    Create leitura_do_enade2014.R

---
+++ leitura_do_enade2014.R
@@ -0,0 +1,9 @@
+#### MQuinho 2019
+### Leitura do ENADE 2014
+### Prof. Dr. Neylson Crepalde
+##############################
+
+library(readr)
+library(dplyr)
+
+enade = read_csv2("https://github.com/neylsoncrepalde/introducao_ao_r/blob/master/dados/enade_2014_amostra.csv?raw=true")
